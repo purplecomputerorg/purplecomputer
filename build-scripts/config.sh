13caf74295effec8fd50540da4d92dd3dfd8f354
Tavi Nathanson <tavi.nathanson@gmail.com>
2025-12-07 16:24:05 -0500

    Proper live boot setup to prevent kernel panic

--- build-scripts/config.sh
+++ build-scripts/config.sh
@@ -9,7 +9,9 @@ ARCH="amd64"
 UBUNTU_MIRROR="http://archive.ubuntu.com/ubuntu"
 
 # Essential packages for nfsroot (installer environment)
-NFSROOT_PACKAGES="linux-image-generic,linux-firmware,casper,systemd,lvm2,parted,e2fsprogs,ntfs-3g,grub-pc-bin,grub-efi-amd64-bin"
+# live-boot: Required for initramfs to boot from squashfs
+# casper: Live system infrastructure
+NFSROOT_PACKAGES="linux-image-generic,linux-firmware,live-boot,casper,systemd,lvm2,parted,e2fsprogs,ntfs-3g,grub-pc-bin,grub-efi-amd64-bin"
 
 # Essential base packages (always needed)
 BASE_PACKAGES="ubuntu-minimal base-files libc6 dpkg apt systemd udev"

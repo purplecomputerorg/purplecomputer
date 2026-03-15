78c2bc4314447b56734017859c6708fe13aef2e2
Tavi Nathanson <tavi.nathanson@gmail.com>
2026-03-14 21:33:05 -0400

    Minor fix

--- purple_tui/rooms/art_room.py
+++ purple_tui/rooms/art_room.py
@@ -1192,6 +1192,8 @@ class ArtHintBar(Static):
 
     def render(self) -> str:
         caps = getattr(self.app, 'caps_text', lambda x: x)
+        if getattr(self.app, '_littles_mode', None):
+            return caps("  Hold Space + arrows to paint.  ")
         return caps("  Press Tab to switch tools. Hold Space + arrows to paint.  ")
 
 
